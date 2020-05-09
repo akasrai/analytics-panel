e1c81046a612efa38133d78433f323e503f4ca90
Akasky <akasky70@gmail.com>
2020-05-09 18:29:22 +0545

    Update mailerUtil.js

--- server/src/utils/mailerUtil.js
+++ server/src/utils/mailerUtil.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 const smptTransport = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: 'ganga00003@gmail.com',
-    pass: 'mynepal12',
+    user: '',
+    pass: '',
   },
 });
 
